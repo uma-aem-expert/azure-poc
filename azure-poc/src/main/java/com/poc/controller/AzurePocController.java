0bdc0bc1f89aa68a78cedb408211ab1ad73e0170
Uma <umcgorai@in.ibm.com>
2024-01-11 01:13:08 +0530

    Message updated

--- azure-poc/src/main/java/com/poc/controller/AzurePocController.java
+++ azure-poc/src/main/java/com/poc/controller/AzurePocController.java
@@ -9,7 +9,7 @@ public class AzurePocController {
 	@GetMapping("/message")
 	public String getMessage() {
 		
-		return "This is the another build";
+		return "Check the automated build!!";
 	}
 
 }
