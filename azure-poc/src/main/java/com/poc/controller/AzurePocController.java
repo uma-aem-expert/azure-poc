94be4597001b48132bb2a5f42e2fa28686a22fe7
Uma <umcgorai@in.ibm.com>
2024-01-10 23:30:57 +0530

    added new class

---
+++ azure-poc/src/main/java/com/poc/controller/AzurePocController.java
@@ -0,0 +1,15 @@
+package com.poc.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AzurePocController {
+	
+	@GetMapping("/message")
+	public String getMessage() {
+		
+		return "This is the POC message";
+	}
+
+}
